d13326debc68feaf5120ab63daa0d2ae35301602
modolee_dev <modolee.si@hanmail.net>
2021-08-05 09:29:26 +0900

    add test.ts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Web Project Backend
+
+- web project
+- web server
+- web client
---
+++ test.ts
@@ -0,0 +1,10 @@
+class testClass {
+    helloWorld() {
+        const helloText = 'HelloWorld';
+        
+        console.log(`print log4 : ${helloText}`);
+        console.log(`print log1 : ${helloText}`);
+        console.log(`print log2 : ${helloText}`);
+        console.log(`print log3 : ${helloText}`);
+    }
+}
